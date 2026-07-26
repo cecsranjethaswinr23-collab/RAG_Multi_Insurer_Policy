127237cc3e0d228478d28897f2f517f21bb3905c
Ranjeth Aswin Ravindran <cecsranjethaswinr23@gmail.com>
2026-07-26 23:20:48 +0530

    UI modified

--- app.py
+++ app.py
@@ -5,43 +5,90 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-from RAG_LLM_Promot import llm_prompt
+from RAG_LLM_Prompt import llm_prompt
 
-# 1. Load environment variables from .env
-load_dotenv()
+# dependancies
+#---------------------------------------------------------------------------------------------------------->
 
+load_dotenv() #Load environment variables from .env
+
+# webpage name and icon
 st.set_page_config(
-    page_title="Multi-Insurer Policy Assistant",
-    page_icon="🛡️",
+    page_title="PolicyLens_AI",
+    page_icon="💎",
     layout="wide"
 )
+
+# Sidebar
+# About title
+st.sidebar.title("About")
+
+# sidebar markdown 1
+st.sidebar.markdown("""
+### 🎯 WHAT IT DOES:
+This Retrieval-Augmented Generation (RAG) application search and answer queries across multiple insurance policies documents seemlessly. 
+The system retrieves relevant context while enabling dynamic provider-level filtering through an interactive Streamlit dashboard, and
+it has the data of multiple Insurance companies health insurance policies and a company's motor vehicle insurance policies.
+""")
+
+# sidebar markdown 2
+companies_name=[
+    "Care (Health Insurance)",
+    "HDFC (Health Insurance)",
+    "Star (Health Insurance)",
+    "United India(vehicle insurance)"
+    ]
+st.sidebar.markdown("""
+#### 🫠 PROCEDURE:
+You can filter the specific insurance you want to question about or else conclusively fetch information from all the insurance policies 
+data to get the the information you need. Before using the Application refer the policies and their wordings to understand how this 
+application can be questioned to get right responses for you. The policies of insurance companies are
+""")
+for company in companies_name:
+    st.sidebar.markdown(f"- ***{company}***")
+
+
+st.sidebar.subheader("🛠️ Tech Stack")
+tech_stack = [
+    "Python",
+    "Streamlit",
+    "Langchain",
+    "FAISS",
+    "Gemini AI"
+]
+for tech in tech_stack:
+    st.sidebar.markdown(f"- **{tech}**")
+
+# github link bar
+st.sidebar.subheader("🔗 Source Code")
+st.sidebar.link_button("💻 Go to GitHub Repository", "https://github.com/cecsranjethaswinr23-collab/SwiftParseAI_Tabletop_AI_agent", use_container_width=True)
+
+# author bar
+st.sidebar.markdown("### 👨‍💻 Developed By")
+st.sidebar.markdown("**Ranjeth Aswin Ravindran**")
+st.sidebar.caption("Data Scientist")
+
+# Email bar
+st.sidebar.markdown("""
+📧 **Contact:** cecsranjethaswinr23@gmail.com
+""")
+# end of about section
+# ------------------------------------------------------------------------------------------------------------------------------------
 
 st.title("🛡️ Multi-Insurer Health Policy Assistant")
 st.caption("Compare quantitative sublimits, coverage terms, and exclusions across policy providers.")
 
 # 2. Cache resources so FAISS loads into memory only once
-@st.cache_resource
-def init_rag_system():
-    # MUST match the exact embedding model used in ingest.ipynb
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    
-    # Load saved FAISS store from disk
-    vectorstore = FAISS.load_local(
-        "faiss_index",
-        embeddings,
-        allow_dangerous_deserialization=True
-    )
-    
-    # Initialize Gemini Flash LLM
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-3.5-flash",
-        temperature=0.1  # Low temperature for strict factual accuracy
-    )
+
+def rag_system():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") # embedding model
+    vectorstore = FAISS.load_local("faiss_index",embeddings,allow_dangerous_deserialization=True) # loading saved FAISS
+    llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash",temperature=0.1)
     
     return vectorstore, llm
 
 try:
-    vectorstore, llm = init_rag_system()
+    vectorstore, llm = rag_system()
     st.sidebar.success("✅ FAISS Vector Index Loaded")
 except Exception as e:
     st.error(f"Failed to load vector store: {e}. Did you run `ingest.ipynb` first?")
@@ -51,7 +98,24 @@ except Exception as e:
 st.sidebar.header("Options")
 company_filter = st.sidebar.selectbox(
     "Filter by Provider:",
-    ["All Insurers", "Star Health", "Care Health", "HDFC ERGO"] # Customize based on your PDFs
+["All Insurers",
+ 'Care Senior Health',
+ 'Care Supreme',
+ 'Care Ultimate Joy',
+ 'Hdfc Equicover Health',
+ 'Hdfc Pradhan Mantri Suraksha Bima Yojana',
+ 'Hdfc Women Suraksha',
+ 'Star Comprehensive Policy',
+ 'Star Medi Classic Insurance Policy',
+ 'Star Senior Citizen Policy',
+ 'Star Women Policy',
+ 'Ui Commercial Vehicles 3Rd Party',
+ 'Ui Commercial Vehicles Cover',
+ 'Ui Private Bike 1Yr Od 3Yr Tp',
+ 'Ui Private Bike 1Yr Od 5Yr Tp',
+ 'Ui Private Bike Only Tp',
+ 'Ui Private Car Only Tp',
+ 'Ui Scooter Own Damage'] # Customize based on your PDFs
 )
 
 
@@ -76,8 +140,10 @@ if user_query:
         response = chain.invoke({"context": context_str, "question": user_query})
 
     # 6. Display Response
-    st.markdown("### 🤖 Answer")
-    st.write(response.content)
+    st.markdown("### 🤖 Response")
+    answer_text = response.content[0]["text"] if isinstance(response.content, list) else response.content
+    st.markdown(answer_text)
+
 
     # 7. Expandable Source Citations (Recruiter Delight)
     with st.expander("🔍 View Retrieved Context Chunks & Page Numbers"):
